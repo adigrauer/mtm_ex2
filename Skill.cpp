91e5fc0cf83504b0922b01851a788e9b995dc00b
adigrauer <adigrauer@campus.technion.ac.il>
2021-12-24 13:37:28 +0200

    update

--- Skill.cpp
+++ Skill.cpp
@@ -1,14 +1,57 @@
 #include "Skill.h"
+#include <string>
+#include <fstream>
+#include <iostream>
+
+using std::ostream;
+using std::cout;
+using std::endl;
+using std::string;
+using namespace mtm;
 
 namespace mtm {
 
     Skill::Skill (int id_skill, string name, unsigned int required_points, int strong): 
-        id_skill(id_skill), required_points(required_points), strong(strong)
+        id_skill(id_skill), required_points(required_points), strong(strong), name(name)
+    {}
+
+    int Skill::getId () const
     {
-        string new_name; // i think we nedd to copy the name 
+        return id_skill;
     }
 
-    Skill::~Skill ()
+    string Skill::getName () const
     {
+        return name;
+    }
+
+    unsigned int Skill::getRequiredPoints () const
+    {
+        return required_points;
+    }
+
+    bool operator< (const Skill& skill_a, const Skill& skill_b)
+    {
+        return skill_a.id_skill < skill_b.id_skill;
+    }
+
+    bool operator== (const Skill& skill_a, const Skill& skill_b)
+    {
+        return skill_a.id_skill == skill_b.id_skill;
+    }
+
+    ostream& operator<< (ostream& os, const Skill& skill)
+    {
+        os << skill.name << "level:" << skill.strong << endl;
+        return os;
     }
 }
+
+int main() {
+    Skill s1(1, "C++", 0, 10);
+    cout << s1;
+    return 0;
+}
+
+    
+

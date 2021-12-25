64d7ec8f910bfbbe59f3daca0a28e9d3734878b0
adigrauer <adigrauer@campus.technion.ac.il>
2021-12-25 12:20:20 +0200

    add notes to h file

--- Skill.h
+++ Skill.h
@@ -11,6 +11,8 @@ using std::endl;
 using std::string;
 
 namespace mtm {
+
+    /* Skill class declaration */
     class Skill {
     private: 
         int id_skill;
@@ -18,20 +20,40 @@ namespace mtm {
         unsigned int required_points;
         int strong;
     public:
+        /* Skill C'tor */
         Skill(int id_skill, string name, unsigned int required_points, int strong);
+        
+        /* Skill copy C'tor */
         Skill(const Skill& skill) = default;
+        
+        /* Skill D'tor */
         ~Skill () = default;
+
+        /* getId: return copy of skill's id.
+        return-
+        int id */
         int getId () const;
+        
+        /* getName: return copy of skill's name.
+        return-
+        string name */
         string getName () const;
+        
+        /* getRequiredPoints: return the rwquired points of a skill.
+        return-
+        unsigned int required points */
         unsigned int getRequiredPoints () const;
-        //print
+
+        /* friens bool operators */
         friend bool operator< (const Skill& skill_a, const Skill& skill_b);
         friend bool operator== (const Skill& skill_a, const Skill& skill_b);
         friend ostream& operator<< (ostream& os, const Skill& skill);
+        
+        /* unary operators */
         Skill& operator+= (int point_to_add);
         Skill& operator++ (int);
     };
-    //compere by id 
+    /* binary operators, compare by id */
     bool operator!= (const Skill& skill_a, const Skill& skill_b);
     bool operator<= (const Skill& skill_a, const Skill& skill_b);
     bool operator>= (const Skill& skill_a, const Skill& skill_b);

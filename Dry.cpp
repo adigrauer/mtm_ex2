8b9a84a89ac1927c76a1001de0adbb0f3ddb0792
adigrauer <adigrauer@campus.technion.ac.il>
2022-01-01 12:45:09 +0200

    dry part- part1 passed

--- Dry.cpp
+++ Dry.cpp
@@ -23,15 +23,51 @@ vector<T> slice(std::vector<T> vec, int start, int step, int stop)
     }
     return new_vector;
 }
+
 /*
+void test_dry_1()
+{
+    // this syntax initializes a vector with values a,b,c,d,e
+    vector<char> vec_char {'a', 'b', 'c', 'd', 'e'};
+    // returns vector with values a,c
+    vector<char> vec_sliced_1 = slice(vec_char, 0, 2, 4);
+    cout << "print vec_sliced_1" << endl;
+    for (unsigned int i = 0; i < vec_sliced_1.size(); i++){
+            cout << vec_sliced_1[i] << " ";
+    }
+    cout << endl;
+    // returns vector with values b,c,d,e
+    vector<char> vec_sliced_2 = slice(vec_char, 1, 1, 5);
+    cout << "print vec_sliced_2" << endl;
+    for (unsigned int i = 0; i < vec_sliced_2.size(); i++){
+            cout << vec_sliced_2[i] << " ";
+    }
+    cout << endl;
+    vector<double> vec_double {0.2, 1.2, 2.2, 3.2, 4.2, 5.2, 6.2, 7.2, 8.2};
+    // returns vector with values 0.2, 1.2, 2.2
+    vector<double> vec_sliced_3 = slice(vec_double, 0, 1, 3);
+    cout << "print vec_sliced_3" << endl;
+    for (unsigned int i = 0; i < vec_sliced_3.size(); i++){
+            cout << vec_sliced_3[i] << " ";
+    }
+    cout << endl;
+    //returns vector with values 2.2, 4.2
+    vector<double> vec_sliced_4 = slice(vec_double, 2, 2, 6);
+    cout << "print vec_sliced_4" << endl;
+    for (unsigned int i = 0; i < vec_sliced_4.size(); i++){
+            cout << vec_sliced_4[i] << " ";
+    }
+    cout << endl;
+}
+*/
+
 //q5 part 5.2
 class A {
 public:
     std::vector<int*> values;
     A ();
     ~A ();
-    void add(int x) { values.push_back(new int(x)); }
-
+    void add(int x);
 };
 
 //A class methods
@@ -42,11 +78,19 @@ A::A():
 
 A::~A()
 {
-    for(int it = 0; it < values.size(); ++it){
-        delete values[it];
+    //change to unsigned int from int cause vector.size return unsigned int
+    for(unsigned int it = 0; it < values.size(); ++it){
+        int* temp = values[it];
+        delete temp;
     }
 }
 
+//cant do new in initialization list
+void A::add(int x)
+{
+    int* to_add = new int(x);
+    values.push_back(to_add); 
+}
 
 int main() {
     A a, sliced;
@@ -57,13 +101,7 @@ int main() {
     return 0;
 }
 
-*/
-int main(){
-    // this syntax initializes a vector with values a,b,c,d,e
-    vector<char> vec1 {'a', 'b', 'c', 'd', 'e'};
-    // returns vector with values a,c
-    vector<char> vec_sliced = slice(vec1, 0, 2, 4);
-    // returns vector with values b,c,d,e
-    vector<char> vec_sliced_2 = slice(vec1, 1, 1, 5);
-}
+
+
+
 
